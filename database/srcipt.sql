f199826e26146052c1750f9be48adb7015307244
GerardSerra18 <gerard.serra@estudiants.urv.cat>
2023-05-14 12:07:42 +0200

    Database Imported

---
+++ database/srcipt.sql
@@ -0,0 +1,6 @@
+INSERT INTO `provider` (`provider_name`, `email`, `phone`, `provider_type`, `is_active`, `updated_at`, `created_at`) VALUES
+('Provider 11', 'provider1@example.com', '1234567890', 'Hotel', '1', NOW(), NOW()),
+('Provider 12', 'provider2@example.com', '9876543210', 'Court', '1', NOW(), NOW()),
+('Provider 13', 'provider3@example.com', '4567890123', 'Complement', '0', NOW(), NOW()),
+('Provider 14', 'provider4@example.com', '3210987654', 'Hotel', '1', NOW(), NOW()),
+('Provider 15', 'provider5@example.com', '0123456789', 'Court', '0', NOW(), NOW());
